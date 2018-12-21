4cb02bff3bb19793bbccbd99a33eadcc8599a62b
Kyraja <florian-petersen95@gmx.de>
2018-12-21 16:53:59 +0100

    Improves the update mechanic of the gui plugin. - Initial Event Handling Added

--- src/main/java/net/escendia/gui/model/network/in/PacketConnection.java
+++ src/main/java/net/escendia/gui/model/network/in/PacketConnection.java
@@ -2,6 +2,7 @@ package net.escendia.gui.model.network.in;
 
 import com.google.gson.JsonObject;
 import net.escendia.gui.controll.GUIService;
+import net.escendia.gui.controll.PacketService;
 import net.escendia.gui.model.GlobalScope;
 import net.escendia.gui.model.gui.GeneralGUIData;
 import net.escendia.ioc.InversionOfControl;
@@ -19,6 +20,7 @@ public abstract class PacketConnection extends PacketIn {
         public Init(JsonObject jsonObject) {
             super(jsonObject);
               InversionOfControl.get().build(GUIService.class).initClient(new GeneralGUIData().fromJson(jsonObject.getAsJsonObject(GlobalScope.INITDATA).toString()));
+              InversionOfControl.get().build(PacketService.class).sendPacket(new net.escendia.gui.model.network.out.PacketConnection.Init());
         }
     }
 
